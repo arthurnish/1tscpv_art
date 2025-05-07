46dd60bdb48c4924bc58e6b9fcdb0436e949e87a
arthurnish <rm566274@fiap.com.br>
2025-05-06 22:16:45 -0300

    quarta versao com arquivo media na FuncA

---
+++ media.py
@@ -0,0 +1,13 @@
+# ALTERAÇÃO DE ARQUIVO PARA EXERCÍCIO
+
+# ALTERAÇÃO NA BRANCH MASTER
+
+titulo = "Media de dois valores"
+print(f'{titulo:^30}')
+
+x = float(input('Entre com o valor de x: '))
+y = float(input('Entre com o valor de y: '))
+media = (x+y)/2
+print("O valor de x é", x, "!")
+print('O valor de y é', y)
+print('A media dos valores é', round(media, 3))
